59bc82b5a604a89e6d9d57f85a812fbc8a6990fb
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-09-22 23:13:56 -0300

    feat: udate query

--- banco_dados/login/admin/recebe_delete.php
+++ banco_dados/login/admin/recebe_delete.php
@@ -3,17 +3,17 @@
 require_once "../conexao/conexao.php";
 
 $sql = mysqli_query($con, "SELECT
-u.id_usuario,
-u.no_usuario,
-u.nu_cpf_usuario,
-u.email_usuario,
-p.no_perfil,
-a.descricao_ativo
-FROM 
-tb_usuario as u
-INNER JOIN tb_perfil as p ON
-p.cod_perfil = u.cod_perfil
-INNER JOIN tb_ativo as a ON
-a.cod_ativo = u.cod_ativo;");
+                        u.id_usuario,
+                        u.no_usuario,
+                        u.nu_cpf_usuario,
+                        u.email_usuario,
+                        p.no_perfil,
+                        a.descricao_ativo
+                        FROM 
+                        tb_usuario as u
+                        INNER JOIN tb_perfil as p ON
+                        p.cod_perfil = u.cod_perfil
+                        INNER JOIN tb_ativo as a ON
+                        a.cod_ativo = u.cod_ativo;");
 
 ?>
